7e685eb057c4b061f5600657937ae723ad15d800
SodoTeo <sodolescuteodor@gmail.com>
2021-07-02 13:55:56 +0300

    Update prog.c
    compute the sum of two arrays in parallel using omp

--- prog.c
+++ prog.c
@@ -1,96 +1,36 @@
+//compute the sum of two arrays in parallel 
 #include <stdio.h>      /* input, output    */
 #include <omp.h>        /* openMP library   */
 #include <time.h>       /* measure time */
+#define N 1000000 /* if arrays not static, I'll be overflowing the stack.*/
+int main(void) {
+    double cpu_time_used;
+    double* firstsourcearray = malloc(N * sizeof(double));
+    double* secondsourcearray = malloc(N * sizeof(double));
+    double* thirdarray = malloc(N * sizeof(double));
+    int i;
 
-#define N 1000000     /* if sourcearray not static, I'll be overflowing the stack.*/
-                        /* > ~10^6 elements is a lot for most systems.*/
-
-
-void forloop(void);
-
-
-int
-main(void)    
-{
-  double *sourcearray = malloc(N*sizeof(double));
-  /* worksharing: for loop */
-  forloop();
-
-  return(0);
-}
-
-/*=============================================================*/
-/*=============================================================*/
-
-void forloop(void){
-  /*do a for loop sequentially and in parallel; measure each times */
-
-
-  printf("=====================\n");
-  printf("FOR LOOP\n");
-  printf("=====================\n\n");
-
-  long i;    
-  clock_t start, end;
-  double cpu_time_used;
-
-  static double sourcearray[N];    
-
-
-  /*============*/
-  /*measure time*/
-  /*============*/
-
-  start=clock();
-
-  for (i=0; i<N; i++){
-    sourcearray[i] = ((double) (i)) * ((double) (i))/2.2034872;
-  }
-
-  end = clock();
-  cpu_time_usedn = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-  printf("Non-parallel needed %lf s\n", cpu_time_usedn);
-
-
-
-
-  /*===============*/
-  /*parallel region*/
-  /*===============*/
-omp_set_num_threads(12);
-#pragma omp parallel 
-  /*need to specify num_threads, when OMP_DYNAMIC=true to make sure 12 are used.*/
-  {
-   
-
-    double starttime_omp, endtime_omp;
-    /*time measurement*/
-    starttime_omp=omp_get_wtime();
-
-    int procs, maxt, nt, id;
-
-    procs = omp_get_num_procs();        // number of processors in use
-    maxt = omp_get_max_threads();       // max available threads
-    nt = omp_get_num_threads();
-    id = omp_get_thread_num();
-
-    printf("num threads forloop %d from id %d, procs: %d, maxthrds: %d\n", nt, id, procs, maxt);
-
-
-#pragma omp for  
-    for (i=0; i<N; i++){
-      sourcearray[i] = ((double) (i)) * ((double) (i))/2.2034872;
+    /* Initialize arrays firstsourcearray and secondsourcearray */
+    for (i = 0; i < N; i++) {
+        firstsourcearray[i] = ((double)(i)) * 2.0;
+        secondsourcearray[i] = ((double)(i)) * 3.0;
     }
 
+    /* Compute values of array thirdarray = firstsourcearray+secondsourcearray in parallel. */
+    omp_set_num_threads(12); /*set to 12 cores*/
+    #pragma omp parallel shared(firstsourcearray, secondsourcearray, thirdarray) private(i)
+       {
+        double starttime_omp, endtime_omp;
+        /*time measurement*/
+        starttime_omp = omp_get_wtime();
 
-    endtime_omp = omp_get_wtime();
-    cpu_time_used = ((endtime_omp - starttime_omp)) ;
-
-
-
-  } /* end parallel region */
-   printf("parallel needed %lf s\n", cpu_time_used);
-
-
+        #pragma omp for             
+                for (i = 0; i < N; i++) {
+                    thirdarray[i] = firstsourcearray[i] + secondsourcearray[i];
+                    /*printf("%f\n", thirdarray[i]); --> if you want to print the result of addition (time consuming for printiing)*/
+                }
+                endtime_omp = omp_get_wtime();
+                cpu_time_used = ((endtime_omp - starttime_omp));
+        } /* end parallel region */
+       printf("parallel computation needed %lf s\n", cpu_time_used);
 }
